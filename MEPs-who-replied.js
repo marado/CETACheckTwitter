f778cd51f278296a042f28ca7665ec65c2d53585
Marcos Marado <mindboosternoori@gmail.com>
2017-02-12 15:21:50 +0000

    Filter out MEPs who have already replied

---
+++ MEPs-who-replied.js
@@ -0,0 +1,36 @@
+/*
+ * Since there's no API for the CETA Check, that I know of, this list is (for
+ * now?) manually maintained.
+ * Data: https://stop-ttip.org/cetacheck/
+ * More info: the purpose here is to have on the list the MEPs who *did* reply
+ * to the CETA Check, so we don't need to bother them anymore.
+ * Contries: due to the manual nature of how this file is updated, it currently
+ * only has the MEPs for the countries the website currently supports.
+ */
+var RepliedPortugal = [
+// "CarlosCoelhoPE",	// didn't reply, 11/02
+// "JoseFariaMEP",		// didn't reply, 11/02
+// "JMFernandesEU",		// didn't reply, 11/02
+// "AnaGomesMEP",		// didn't reply, 11/02
+// "marinhoepinto",		// didn't reply, 11/02
+"mmatias_",
+// "NunoMeloCDS",		// didn't reply, 11/02
+// "cmonteiroaguiar",	// didn't reply, 11/02
+// "libertarofuturo",	// didn't reply, 11/02
+// "SofiaHRibeiro",		// didn't reply, 11/02
+// "LilianaMEP",		// didn't reply, 11/02
+//"MJRodriguesEU",		// didn't reply, 11/02
+//"czorrinho",			// didn't reply, 11/02
+//"fernandoruas", 		// didn't reply, 11/02
+];
+
+// Missing from the PT list:
+// * didn't reply
+//   - Ricardo Serr√o Santos		// Not on Twitter
+//   - Manuel Ant√nio dos Santos	// Not on Twitter
+//   - Pedro Silva Pereira			// Not on Twitter
+//	 - Francisco Assis				// Not on Twitter
+//   - Jo√o Ferreira				// Not on Twitter
+// * replied favourably
+//	 - Jo√o Pimenta Lopes
+//   - Miguel Viegas
